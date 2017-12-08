655a2db896d8467d97165d096f08e51c68a73791
SIDDHARTHA <ssiddhartha.1193@gmail.com>
2017-12-07 18:38:28 -0800

    Home page for the website

---
+++ index.php
@@ -0,0 +1,156 @@
+<?php
+include "lib/class.front.php";
+$front = new Front48();
+include "lib/header-home.php";
+
+?>
+
+<div style="position:relative;">
+        <div id="rev_slider_1078_1_wrapper" class="rev_slider_wrapper fullwidthbanner-container   slider_styles space-left space-top span-size30 style-colorbfc" data-alias="classic4export" data-source="gallery" style="margin:0px auto;background-color:transparent;padding:0px;margin-top:0px;margin-bottom:0px;">
+                <!-- START REVOLUTION SLIDER 5.3.0.2 auto mode -->
+                <div id="rev_slider_1078_1" class="rev_slider fullwidthabanner" style="display:none;" data-version="5.3.0.2">
+                    <div class="slotholder"></div>
+                    <ul><!-- SLIDE  -->
+                
+                        <!-- SLIDE 1 -->
+                        <li data-index="rs-3049" data-transition="slideremovedown" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="Power4.easeInOut" data-easeout="Power4.easeInOut" data-masterspeed="2000"    data-rotate="0"  data-saveperformance="off"  data-title="Ken Burns" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">                        
+
+                            <!-- MAIN IMAGE -->
+                            <img src="images/slides/slides1.jpg"  alt=""  data-bgposition="center center" data-kenburns="off" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+                            <!-- LAYERS -->
+
+                            <!-- LAYER NR. 12 -->
+                            <div class="tp-caption title-slide" 
+                                id="slide-3049-layer-1" 
+                                data-x="['left','left','left','left']" data-hoffset="['0','0','0','0']" 
+                                data-y="['middle','middle','middle','middle']" data-voffset="['-77','-77','-77','-77']" 
+                                data-fontsize="['72','72','35','35']"
+                                data-lineheight="['63','63','45','35']"
+                                data-fontweight="['700','700','700','700']"
+                                data-width="none"
+                                data-height="none"
+                                data-whitespace="nowrap"
+                     
+                                data-type="text" 
+                                data-responsive_offset="on"                             
+
+                                data-frames='[{"delay":1000,"speed":2000,"frame":"0","from":"y:-50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+
+                                data-textAlign="['left','left','left','left']"
+                                data-paddingtop="[20,20,20,20]"
+                                data-paddingright="[0,0,0,0]"
+                                data-paddingbottom="[0,0,0,0"
+                                data-paddingleft="[0,0,0,0]"
+
+                                style="z-index: 16; white-space: nowrap;text-transform:left;"><span> QUICK GUIDE TO LEARN</span><br>PHP
+                            </div>
+
+                            <!-- LAYER NR. 13 -->
+                            <div class="tp-caption sub-title" 
+                                id="slide-3049-layer-4" 
+                                data-x="['left','left','left','left']" data-hoffset="['0','0','0','0']" 
+                                data-y="['middle','middle','middle','middle']" data-voffset="['55','55','0','0']"
+                                data-fontsize="['16',16','14','14']" 
+                                data-fontweight="['300','300','300','300']"
+                                data-width="660"
+                                data-height="none"
+                                data-lineheight="['26','26','20','20']"
+                                data-whitespace="nowrap"
+                                
+                                data-type="text" 
+                                data-responsive_offset="on" 
+
+                                data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","speed":2000,"to":"o:1;","delay":1500,"ease":"Power4.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power2.easeInOut"}]'
+                                data-textAlign="['left','left','left','left']"
+                                data-paddingtop="[0,0,0,0]"
+                                data-paddingright="[0,0,0,0]"
+                                data-paddingbottom="[0,0,0,0]"
+                                data-paddingleft="[0,0,0,0]"
+
+                                style="z-index: 17; white-space: nowrap;text-transform:left;">
+                            </div>
+
+                            
+                        </li>
+
+                        <!-- SLIDE 2 -->
+                        <li data-index="rs-3050" data-transition="slideremovedown" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="Power4.easeInOut" data-easeout="Power4.easeInOut" data-masterspeed="2000"    data-rotate="0"  data-saveperformance="off"  data-title="Ken Burns" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">                        
+
+                            <!-- MAIN IMAGE -->
+                            <img src="images/slides/slides3.jpg"  alt=""  data-bgposition="center center" data-kenburns="off" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+                            <!-- LAYERS -->
+
+                            <!-- LAYER NR. 12 -->
+                            <div class="tp-caption title-slide" 
+                                id="slide-3049-layer-2" 
+                                data-x="['left','left','left','left']" data-hoffset="['0','0','0','0']" 
+                                data-y="['middle','middle','middle','middle']" data-voffset="['-77','-77','-77','-77']" 
+                                data-fontsize="['72','72','35','35']"
+                                data-lineheight="['60','60','45','35']"
+                                data-fontweight="['700','700','700','700']"
+                                data-width="none"
+                                data-height="none"
+                                data-whitespace="nowrap"
+                     
+                                data-type="text" 
+                                data-responsive_offset="on"                             
+
+                                data-frames='[{"delay":1000,"speed":2000,"frame":"0","from":"y:-50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+
+                                data-textAlign="['left','left','left','left']"
+                                data-paddingtop="[20,20,20,20]"
+                                data-paddingright="[0,0,0,0]"
+                                data-paddingbottom="[0,0,0,0"
+                                data-paddingleft="[0,0,0,0]"
+
+                                style="z-index: 16; white-space: nowrap;text-transform:left;"><span>OPENING UP A WORLD OF</span><br>EDUCATION
+                            </div>
+
+                            <!-- LAYER NR. 13 -->
+                            <div class="tp-caption sub-title" 
+                                id="slide-3049-layer-5" 
+                                data-x="['left','left','left','left']" data-hoffset="['0','0','0','0']" 
+                                data-y="['middle','middle','middle','middle']" data-voffset="['55','55','0','0']"
+                                data-fontsize="['16',16','14','14']" 
+                                data-fontweight="['300','300','300','300']"
+                                data-width="660"
+                                data-height="none"
+                                data-lineheight="['26','26','20','20']"
+                                data-whitespace="nowrap"
+                     
+                                data-type="text" 
+                                data-responsive_offset="on" 
+
+                                data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","speed":2000,"to":"o:1;","delay":1500,"ease":"Power4.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power2.easeInOut"}]'
+                                data-textAlign="['left','left','left','left']"
+                                data-paddingtop="[0,0,0,0]"
+                                data-paddingright="[0,0,0,0]"
+                                data-paddingbottom="[0,0,0,0]"
+                                data-paddingleft="[0,0,0,0]"
+
+                                style="z-index: 17; white-space: nowrap;text-transform:left;">
+                            </div>
+
+                            
+                        </li>
+
+
+  
+
+                                               
+                    </ul>
+                </div>
+        </div><!-- END REVOLUTION SLIDER -->
+
+		<div style="position:absolute;bottom:120px;width: 100%;background: transparent;text-align: center;">
+		<a class="flat-btn btn_res" href="page.php?page_id=1">Begin Tutorial</a><span class="gap_w"></span><a class="flat-btn btn_res" href="quiz.php">Begin Quiz</a>
+		</div>
+
+		
+<?php
+include "lib/footer-home.php";
+unset($_SESSION["quiz_id"]);
+unset($_SESSION["attempt"]);
+unset($_SESSION["correct"]);
+unset($_SESSION["std_id"]);
+?>
